7205fad65be8a85551b5fa79c586cff3d42a9357
Vidar Tonaas Fauske <vidartf@gmail.com>
2019-10-18 14:18:48 +0100

    Cleanup 0.3.0

--- phoila/_version.py
+++ phoila/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Vidar Tonaas Fauske.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 3, 0, 'final')
+version_info = (0, 3, 0)
 __version__ = ".".join(map(str, version_info))
