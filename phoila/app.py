ee2dd09e527b641cd66b7bcec8935e426a71ddb9
Saul Shanabrook <s.shanabrook@gmail.com>
2019-10-22 13:37:08 -0400

    Update app.py

--- phoila/app.py
+++ phoila/app.py
@@ -103,6 +103,7 @@ class PhoilaApp(ServerApp):
         if not self._dispatching:
             self.kernel_manager.allowed_message_types = [
                 'comm_open',
+                'comm_close',
                 'comm_msg',
                 'comm_info_request',
                 'kernel_info_request',
